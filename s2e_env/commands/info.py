ed1566384cdaf80163a05bda3bfdf3b5fe8f04d4
Vitaly Chipounov <vitaly@cyberhaven.io>
2017-04-15 20:30:45 +0200

    commands: updated info
    Signed-off-by: Vitaly Chipounov <vitaly@cyberhaven.io>

--- s2e_env/commands/info.py
+++ s2e_env/commands/info.py
@@ -52,22 +52,15 @@ class Command(EnvCommand):
     def handle(self, **options):
         # Naively check if S2E has been built (by checking some QEMU binaries
         # exist)
-        s2e_built = len(glob.glob(self.env_path('bin', 'qemu-system-*'))) > 0
+        s2e_built = len(glob.glob(self.install_path('bin', 'qemu-system-*'))) > 0
 
         # Get information on the available images
-        images = {}
-        for img_path in glob.glob(self.env_path('images', '*.json')):
+        images = []
+        for img_path in glob.glob(self.image_path('*', '*.json')):
             # We only care about the file name, not the extension (first index)
             # Remove the leading '.' from the hidden file
-            name = os.path.splitext(os.path.basename(img_path))[0][1:]
-
             with open(img_path, 'r') as f:
-                images[name] = json.load(f)
-
-                # Make creation timestamp human-readable
-                create_time = images[name]['creation_time']
-                images[name]['creation_time'] = \
-                    _make_date_human_readable(create_time)
+                images.append(json.load(f))
 
         # Get information on the available projects
         projects = {}
