721f2aac7eef82787c4fc709aa6340e79e830841
Vitaly Chipounov <vitaly@cyberhaven.io>
2017-04-17 18:43:22 +0200

    commands/server: imported cgc interface
    Signed-off-by: Vitaly Chipounov <vitaly@cyberhaven.io>

---
+++ s2e_env/commands/server/coverage.py
@@ -0,0 +1,178 @@
+"""
+Copyright (c) 2017 Cyberhaven
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import json
+import logging
+import os
+
+from .queueprocessor import QueueProcessor, terminating
+
+logger = logging.getLogger(__name__)
+
+BB_COVERAGE = 0
+TB_COVERAGE = 1
+
+
+class Coverage(QueueProcessor):
+    def __init__(self):
+        QueueProcessor.__init__(self)
+        # Split Bb and Tb coverage for stats purposes.
+        # The DB stores mixed info.
+        self._bb_coverage = {}
+        self._tb_coverage = {}
+        self._static_info = {}
+        self._summary_updated = False
+        self._summary = {}
+
+    def update_summary(self):
+
+        bb_summary = {}
+        bb_aggregate = 0
+        bb_static_aggregate = 0
+        for module, coverage in self._bb_coverage.iteritems():
+            l = len(coverage)
+            bb_summary[module] = l
+            bb_aggregate += l
+            bb_static_aggregate += self._static_info[module]['static_bbs']
+
+        tb_aggregate = 0
+        for module, coverage in self._tb_coverage.iteritems():
+            l = len(coverage)
+            tb_aggregate += l
+
+        coverage = {
+            'breakdown': bb_summary,
+            'covered_tbs_total': tb_aggregate,
+            'covered_bbs_total': bb_aggregate,
+            'available_bbs_total': bb_static_aggregate
+        }
+
+        logger.info('Updating coverage summary %s', str(coverage))
+
+        self._summary = coverage
+        self._summary_updated = True
+
+    def compute_bb_diff(self, data, is_tb=False):
+        result = {}
+
+        if is_tb:
+            actual_cov = self._tb_coverage
+        else:
+            actual_cov = self._bb_coverage
+
+        for module, coverage in data.iteritems():
+            if module not in actual_cov.keys():
+                result[module] = coverage
+                actual_cov[module] = set()
+
+            data_set = set()
+            if is_tb:
+                cdata = coverage
+            else:
+                cdata = coverage['covered_blocks']
+
+            for bb in cdata:
+                # Both BB and TB coverage data items begin with start_pc and end_pc
+                # (start_pc, end_pc)
+                t = (bb[0], bb[1])
+                data_set.add(t)
+
+            result[module] = data_set.difference(actual_cov[module])
+
+        return result
+
+    def is_covered(self, module, bb):
+        bbs = self._bb_coverage.get(module, {})
+        tbs = self._tb_coverage.get(module, {})
+
+        return bb in bbs or bb in tbs
+
+    def process_coverage(self, _, coverage_file, coverage_type):
+        with open(coverage_file) as fp:
+            data = json.load(fp)
+
+        os.remove(coverage_file)
+
+        diff = self.compute_bb_diff(data, coverage_type == TB_COVERAGE)
+
+        logger.info('Processing coverage data diff: %s', str(diff))
+        for module, coverage in diff.iteritems():
+            logger.info('module: %s coverage: %s ', module, str(coverage))
+
+            for bb in coverage:
+                # In principle, we could have a diff over the union of bbs
+                # and tbs, but it's just simpler to filter out here.
+                if self.is_covered(module, bb):
+                    continue
+
+                logger.info("Found new bb: %s %x:%x", module, bb[0], bb[1])
+
+                self._summary_updated = False
+
+            if coverage_type == TB_COVERAGE:
+                self._tb_coverage[module] = self._tb_coverage[module].union(coverage)
+            else:
+                self._bb_coverage[module] = self._bb_coverage[module].union(coverage)
+                self._static_info[module] = {
+                    'static_bbs': data[module]['static_bbs']
+                }
+
+        if not self._summary_updated:
+            self.update_summary()
+
+    def run(self):
+        logger.info('Starting coverage thread')
+
+        while not terminating():
+
+            try:
+                # Need timeout to avoid getting stuck on termination
+                item = self._queue.get(True, 2)
+            except Exception:
+                continue
+
+            (analysis, coverage_file, coverage_type, cb) = item
+
+            try:
+                self.process_coverage(analysis, coverage_file, coverage_type)
+
+                if cb is not None:
+                    cb(analysis, item)
+            except Exception as e:
+                # Log the errors, don't crash the thread
+                logger.error(e, exc_info=True)
+
+        logger.info('Terminating coverage thread')
+
+    def queue_coverage(self, analysis, coverage_file, coverage_type, cb):
+        logger.info('Queuing coverage file ' + coverage_file)
+        item = (analysis, coverage_file, coverage_type, cb)
+        self._queue.put(item)
+
+    def get_tb_coverage(self):
+        return self._tb_coverage
+
+    def get_summary(self):
+        return self._summary
+
+    def get_global_stats(self):
+        return self.get_global_stats()
---
+++ s2e_env/commands/server/stats.py
@@ -0,0 +1,93 @@
+"""
+Copyright (c) 2017 Cyberhaven
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import logging
+from threading import Thread
+from Queue import Queue
+from .queueprocessor import terminating
+
+logger = logging.getLogger(__name__)
+
+
+class CGCStats(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self._queue = Queue()
+        self._stats = {}
+        self._global_stats = {}
+
+    def process_stats(self, _, data):
+        # Update per-module stats
+        stats = data.get('stats', {})
+        for module, mdata in stats.iteritems():
+            upd = self._stats.get(module, {})
+            upd['called_random'] = upd.get('called_random', False) or mdata.get('called_random', False)
+            upd['random_branches_pc'] = list(
+                set(upd.get('random_branches_pc', [])).union(mdata.get('random_branches_pc', []))
+            )
+            self._stats[module] = upd
+
+        # Update global stats
+        gs = data.get('global_stats', {})
+
+        self._global_stats['states'] = self._global_stats.get('states', 0) + gs.get('states_delta', 0)
+
+        max_stats = [
+            'max_completed_path_depth', 'max_path_depth', 'used_seeds',
+            'recipe_count', 'invalid_recipe_count', 'cfg_bb_count', 'model_count'
+        ]
+
+        for s in max_stats:
+            self._global_stats[s] = max(self._global_stats.get(s, 0), gs.get(s, 0))
+
+        aggregated_stats = [
+            'completed_paths', 'completed_seeds',
+            'failed_recipe_tries', 'successful_recipe_tries'
+        ]
+
+        for s in aggregated_stats:
+            self._global_stats[s] = self._global_stats.get(s, 0) + gs.get(s, 0)
+
+    def run(self):
+        logger.info('Starting stats collection thread')
+
+        while not terminating:
+            try:
+                # Need timeout to avoid getting stuck on termination
+                analysis, data = self._queue.get(True, 2)
+            except Exception:
+                continue
+
+            try:
+                self.process_stats(analysis, data)
+            except Exception as e:
+                # Log the errors, don't crash the thread
+                logger.error(e, exc_info=True)
+
+        logger.info('Terminating stats collection thread')
+
+    def queue_stats(self, analysis, data):
+        data = (analysis, data)
+        self._queue.put(data)
+
+    def get_global_stats(self):
+        return self._global_stats
