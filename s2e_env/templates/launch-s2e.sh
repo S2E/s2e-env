766a38dfed0c6cb712be2e7af845a324fd77d607
Adrian Herrera <adrian.herrera@epfl.ch>
2017-05-03 09:20:56 +0000

    launch-s2e.sh: check for debug build

--- s2e_env/templates/launch-s2e.sh
+++ s2e_env/templates/launch-s2e.sh
@@ -25,6 +25,11 @@ export S2E_UNBUFFERED_STREAM=1
 
 if [ "x$DEBUG" != "x" ]; then
 
+if [ ! -d "$BUILD_DIR/qemu-$BUILD" ]; then
+    echo "No debug build found. Please run \`\`s2e build -g\`\`"
+    exit 1
+fi
+
 QEMU="$BUILD_DIR/qemu-$BUILD/{{ arch }}-softmmu/qemu-system-{{ arch }}"
 LIBS2E="$BUILD_DIR/libs2e-$BUILD/{{ arch }}-s2e-softmmu/libs2e.so"
 
