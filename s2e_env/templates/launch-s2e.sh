fdc41dfcd287ea4c2ed8f333753d903d24118da7
Vitaly Chipounov <vitaly@cyberhaven.io>
2018-01-14 14:37:00 +0100

    templates: disable graphics output in multi-threaded mode
    Signed-off-by: Vitaly Chipounov <vitaly@cyberhaven.io>

--- s2e_env/templates/launch-s2e.sh
+++ s2e_env/templates/launch-s2e.sh
@@ -26,6 +26,12 @@ export S2E_SHARED_DIR=$INSTALL_DIR/share/libs2e
 export S2E_MAX_PROCESSES=1
 export S2E_UNBUFFERED_STREAM=1
 
+if [ $S2E_MAX_PROCESSES -gt 1 ]; then
+    # Multi-threaded mode does not support graphics output, so we override
+    # whatever settings were there before.
+    export GRAPHICS=-nographic
+fi
+
 if [ "x$DEBUG" != "x" ]; then
 
 if [ ! -d "$BUILD_DIR/qemu-$BUILD" ]; then
