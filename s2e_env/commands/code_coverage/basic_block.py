65a1f2ff6a372d777035929ad67ee136495efa35
Adrian Herrera <adrian.herrera02@gmail.com>
2018-02-15 12:34:15 +1100

    basic_block: added support for drcov format
    The drvov format is compatible with that used by the IDA Pro Lighthouse plugin
    (https://github.com/gaasedelen/lighthouse/)

--- s2e_env/commands/code_coverage/basic_block.py
+++ s2e_env/commands/code_coverage/basic_block.py
@@ -28,6 +28,7 @@ import json
 import itertools
 import logging
 import os
+import struct
 
 from s2e_env.command import ProjectCommand, CommandError
 from . import get_tb_files, get_tb_state, parse_tb_file
@@ -99,12 +100,21 @@ class BasicBlockCoverage(ProjectCommand):
 
     This subcommand requires one of IDA Pro, Radare2 or Binary Ninja to use as
     a disassembler.
+
+    Basic block coverage can be generated in one of two formats:
+
+     * A single JSON file, where the basic block coverage across **all** states
+       is aggregated into this single file.
+     * Multiple drcov (http://dynamorio.org/docs/page_drcov.html) files, where
+       each file corresponds to a different S2E state. The drcov format used
+       is compatible with the IDA Pro Lighthouse plugin
+       (https://github.com/gaasedelen/lighthouse).
     """
 
     help = 'Generate a basic block coverage report. This requires IDA Pro, '  \
            'Radare2 or Binary Ninja as a disassembler.'
 
-    RESULTS = 'Basic block coverage saved to {bb_file}\n\n'             \
+    RESULTS = 'Basic block coverage saved to {bb_loc}\n\n'              \
               'Statistics\n'                                            \
               '==========\n\n'                                          \
               'Total basic blocks: {num_bbs}\n'                         \
@@ -120,6 +130,8 @@ class BasicBlockCoverage(ProjectCommand):
 
         # Get translation block coverage information for each module
         for module, _ in modules:
+            module_path = os.path.join(target_dir, module)
+
             # Check if a cached version of the disassembly information exists.
             # If it does, then we don't have to disassemble the binary (which
             # may take a long time for large binaries)
@@ -128,7 +140,6 @@ class BasicBlockCoverage(ProjectCommand):
             # If no cached .disas file exists, generate a new one using the
             # given disassembler and cache the results
             if not disas_info:
-                module_path = os.path.join(target_dir, module)
                 disas_info = self._get_disassembly_info(module_path)
                 if not disas_info:
                     raise CommandError('No disassembly information found')
@@ -148,14 +159,23 @@ class BasicBlockCoverage(ProjectCommand):
 
             # Write the basic block coverage information to disk.
             #
-            # Combine all the basic block coverage information (across all
-            # states) into a single JSON file.
-            bb_coverage_file = self._save_basic_block_coverage(module,
-                                                               bb_coverage,
-                                                               total_bbs,
-                                                               num_covered_bbs)
+            # If we are using drcov format, each state's basic block coverage
+            # is written to a separate drcov file.
+            #
+            # Otherwise combine all the basic block coverage information
+            # (across all states) into a single JSON file.
+            if options['drcov']:
+                bb_coverage_loc = self._save_drcov(module_path,
+                                                   disas_info['base_addr'],
+                                                   disas_info['end_addr'],
+                                                   bb_coverage)
+            else:
+                bb_coverage_loc = self._save_basic_block_coverage(module,
+                                                                  bb_coverage,
+                                                                  total_bbs,
+                                                                  num_covered_bbs)
 
-            logger.success(self.RESULTS.format(bb_file=bb_coverage_file,
+            logger.success(self.RESULTS.format(bb_loc=bb_coverage_loc,
                                                num_bbs=total_bbs,
                                                num_covered_bbs=num_covered_bbs,
                                                percent=num_covered_bbs / total_bbs))
@@ -298,3 +318,91 @@ class BasicBlockCoverage(ProjectCommand):
             json.dump(bbs_json, f)
 
         return bb_coverage_file
+
+    ####################
+    # drcov generation #
+    ####################
+
+    DRCOV_HEADER = 'DRCOV VERSION: 2\n'                                         \
+                   'DRCOV FLAVOR: S2E\n'                                        \
+                   'Module Table: version 2, count 1\n'                         \
+                   'Columns: id, base, end, entry, checksum, timestamp, path\n'
+
+    DRCOV_MOD_FORMAT = '{zero:3d}, {base:#016x}, {end:#016x}, {zero:#016x}, '   \
+                       '{zero:#08x}, {zero:#08x}, {path}\n'
+
+    # Based off the C structure as used by drcov:
+    #
+    # typedef struct _bb_entry_t {
+    #     uint start;   /* offset of bb start from the image base. */
+    #     ushort size;
+    #     ushort mod_id;
+    # } bb_entry_t;
+    DRCOV_BB_FORMAT = 'IHH'
+
+    def _save_drcov(self, module_path, module_base, module_end, basic_blocks):
+        """
+        Write the basic block coverage information to multiple drcov files.
+        Each drcov file corresponds to an individual S2E state.
+
+        The drcov format is based off that used by the Lighthouse plugin for
+        IDA Pro (https://github.com/gaasedelen/lighthouse).
+
+        Note that the drcov reports only contain basic block coverage
+        information for the given module - coverage information for shared
+        libraries, etc. are **not** included.
+
+        Args:
+            module_path: Path to the module that basic block coverage has been
+            generated for.
+            basic_blocks: Dictionary mapping state IDs to covered basic blocks.
+
+        Returns:
+            The path to the directory storing the drcov files.
+        """
+        drcov_dir = self.project_path('s2e-last', 'drcov')
+        if os.path.isdir(drcov_dir):
+            raise CommandError('drcov directory %s already exists' % drcov_dir)
+
+        os.mkdir(drcov_dir)
+
+        module = os.path.basename(module_path)
+
+        for state, bbs in basic_blocks.items():
+            drcov_filename = '%s_coverage_%s.drcov' % (module, state)
+            drcov_file = os.path.join(drcov_dir, drcov_filename)
+
+            with open(drcov_file, 'wb') as f:
+                f.write(self.DRCOV_HEADER)
+
+                # Each drcov module entry is formatted as follows:
+                #
+                # 1. module ID
+                # 2. base address
+                # 3. end address
+                # 4. path to module on disk
+                #
+                # Because we only produce drcov information for the given
+                # module, there is only a single module with an ID of 0
+                f.write(self.DRCOV_MOD_FORMAT.format(zero=0,
+                                                     base=module_base,
+                                                     end=module_end,
+                                                     path=module_path))
+
+                # Each drcov basic block entry is formatted as follows:
+                #
+                # 1. basic block start address (relative to the module base
+                # address)
+                # 2. basic block size
+                # 3. module ID that the basic block belongs to
+                #
+                # Because there is only a single module entry, the module ID
+                # will always be 0
+                f.write('BB Table: %d bbs\n' % len(bbs))
+                for bb in bbs:
+                    f.write('%s' % struct.pack(self.DRCOV_BB_FORMAT,
+                                               bb.start_addr - module_base,
+                                               bb.end_addr - bb.start_addr,
+                                               0))
+
+        return drcov_dir
