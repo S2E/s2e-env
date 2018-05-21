213ca64d62b3ce576a806332c987ad8db96f88f4
Vitaly Chipounov <vitaly@cyberhaven.io>
2018-05-21 21:14:12 +0200

    setup: fixed pyelftools version
    Signed-off-by: Vitaly Chipounov <vitaly@cyberhaven.io>

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         'jinja2',
         'pefile',
         'psutil',
-        'pyelftools==0.24s2e',
+        'pyelftools==0.24+s2e',
         'python-magic',
         'pyyaml',
         'requests',
@@ -54,7 +54,7 @@ setup(
         'pytrie'
     ],
     packages=find_packages(),
-    dependency_links=['git+https://github.com/S2E/pyelftools.git#egg=pyelftools-0.24s2e'],
+    dependency_links=['git+https://github.com/S2E/pyelftools.git#egg=pyelftools-0.24+s2e'],
     include_package_data=True,
     package_data={
         's2e_env': [
