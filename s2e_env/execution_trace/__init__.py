93adf7a7e73c4c640c319a6d7d81b40364090c06
Vitaly Chipounov <vitaly@cyberhaven.io>
2018-06-29 16:01:01 +0200

    execution_trace: do not sort them by timestamp
    Signed-off-by: Vitaly Chipounov <vitaly@cyberhaven.io>

--- s2e_env/execution_trace/__init__.py
+++ s2e_env/execution_trace/__init__.py
@@ -181,10 +181,6 @@ class ExecutionTraceParser(object):
                 logger.debug('Parsing %s', trace_file_path)
                 self._parse_trace_file(trace_file, path_ids)
 
-        # Sort the execution trace for each state
-        for execution_trace in self._execution_traces.values():
-            execution_trace.sort(key=lambda x: x[0].timestamp)
-
         # If a list of path IDs is given, we will return these states plus
         # their parents.
         #
