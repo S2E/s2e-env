51c270ef716dfff81cc7acfa319b3aa6259b4949
Adrian Herrera <adrian.herrera02@gmail.com>
2018-12-01 12:57:48 +1100

    tests: added DLL analysis and project tests

--- tests/commands/project_creation/test_windows_project.py
+++ tests/commands/project_creation/test_windows_project.py
@@ -28,7 +28,7 @@ from unittest import TestCase
 from mock import MagicMock
 
 from s2e_env.commands.new_project import _extract_inf_files
-from s2e_env.commands.project_creation.windows_project import WindowsProject, WindowsDriverProject
+from s2e_env.commands.project_creation.windows_project import WindowsProject, WindowsDLLProject, WindowsDriverProject
 from . import DATA_DIR
 
 
@@ -88,6 +88,10 @@ SCANNER_INF_PATH = os.path.join(DATA_DIR, SCANNER_INF)
 SCANNER_SYS_PATH = os.path.join(DATA_DIR, SCANNER_SYS)
 SCANNER_USER_EXE_PATH = os.path.join(DATA_DIR, SCANNER_USER_EXE)
 
+MYPUTS_DLL = u'myputs.dll'
+
+MYPUTS_DLL_PATH = os.path.join(DATA_DIR, MYPUTS_DLL)
+
 
 class WindowsProjectTestCase(TestCase):
     def setUp(self):
@@ -98,6 +102,10 @@ class WindowsProjectTestCase(TestCase):
         self._windows_driver_project = WindowsDriverProject()
         self._windows_driver_project._select_image = MagicMock(return_value=WINDOWS_7SP1_X64_IMAGE_DESC)
         self._windows_driver_project._env_dir = MagicMock(return_value=gettempdir())
+
+        self._windows_dll_project = WindowsDLLProject()
+        self._windows_dll_project._select_image = MagicMock(return_value=WINDOWS_7SP1_X64_IMAGE_DESC)
+        self._windows_dll_project._env_dir = MagicMock(return_value=gettempdir())
 
     def test_empty_xpsp3pro_project_config(self):
         """Test empty Windows XP SP3 project creation."""
@@ -133,6 +141,7 @@ class WindowsProjectTestCase(TestCase):
         self.assertFalse(config['use_fault_injection'])
 
     def test_scanner_driver_7sp1ent_x64_project_config(self):
+        """Test x64 driver project creation."""
         file_paths = _extract_inf_files(SCANNER_INF_PATH)
         args = {
             'target_files': list(set([SCANNER_INF_PATH] + file_paths)),
@@ -161,3 +170,34 @@ class WindowsProjectTestCase(TestCase):
         self.assertFalse(config['use_seeds'])
         self.assertFalse(config['use_recipes'])
         self.assertFalse(config['processes'])
+
+    def test_myputs_dll_7sp1ent_x64_project_config(self):
+        """Test x64 DLL project creation."""
+        args = {
+            'target_files': [MYPUTS_DLL_PATH],
+            'target_args': ['MyPuts'],
+            'target_arch': 'x86_64',
+        }
+
+        config = self._windows_dll_project._make_config(**args)
+
+        # Assert that we have actually created a Windows project
+        self.assertEqual(config['project_type'], 'windows')
+
+        # Assert that the target is the one given
+        self.assertEqual(config['target_path'], MYPUTS_DLL_PATH)
+        self.assertEqual(config['target_arch'], 'x86_64')
+        self.assertItemsEqual(config['target_files'], [MYPUTS_DLL_PATH])
+
+        # Assert that the x86_64 Windows 7 image was selected
+        self.assertDictEqual(config['image'], WINDOWS_7SP1_X64_IMAGE_DESC)
+
+        # Assert that the DLL entry point is the one that we provided
+        self.assertEqual(config['target_args'], ['MyPuts'])
+
+        # Verify static analysis results
+        self.assertItemsEqual(config['dll_exports'], [u'MyPuts'])
+
+        # Disabled by default
+        self.assertFalse(config['processes'])
+        self.assertFalse(config['use_seeds'])
